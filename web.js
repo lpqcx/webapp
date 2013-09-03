032eacf138e1a6ef0048bf6b1e08aac2666b953d
lpqcx <lpq1990@gmail.com>
2013-09-03 00:10:35 +0000

    initial project version

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+var app = express.createServer(express.logger());
+
+var buf = fs.readFileSync('index.html');
+
+app.get('/', function(request, response) {
+  response.send(buf.toString()
+);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
