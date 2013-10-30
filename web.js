1525380dfb0ccfea05e9cf4a68772b749e5e647c
lpqcx <lpq1990@gmail.com>
2013-10-30 20:26:55 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+app.use('/img',express.static(__dirname + '/img'));
+app.use('/js',express.static(__dirname + '/js'));
+app.use('/css',express.static(__dirname + '/css'));
 
 var buf = fs.readFileSync('index.html');
 
